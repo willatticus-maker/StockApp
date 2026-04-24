ae9995751584a0efa79f68202a6e52cc3592f741
willatticus-maker <will.atticus@gmail.com>
2026-04-24 09:24:17 -0400

    Initial Commit

---
+++ StockApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockApp
+//
+//  Created by Will Fk on 4/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Will Fk on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
