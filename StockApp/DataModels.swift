69fdea239d94cb30df89fc2d777f4e41e89a3b86
willatticus-maker <will.atticus@gmail.com>
2026-04-27 09:28:38 -0400

    started data modeling

---
+++ StockApp/DataModels.swift
@@ -0,0 +1,16 @@
+//
+//  DataModels.swift
+//  StockApp
+//
+//  Created by Will Fk on 4/27/26.
+//
+
+import Foundation
+
+struct MetaData :  Codable{
+  var  Symbol : String
+    }
+
+struct TimeSeries: Codable {
+    
+}
