d9de88f7cdb5581fc4d8f9ecb189a97db373d056
BT_C09_20 <mobileappdev@bths.edu>
2026-04-27 09:16:00 -0400

    Fixed NetworkClient Commit

--- StockApp/NetworkClient.swift
+++ StockApp/NetworkClient.swift
@@ -6,28 +6,21 @@
 //
 import SwiftUI
 
-struct DataManager{
-    Vstack {
-    .task {
-        let urlStr = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=10&apikey=26YEL3PQC5ZHFB1P"
-        let url: URL? = URL(string: urlStr)
-        guard let urlUnwrapped = url else {
-            return
-        }
-        do {
-            let (data, response) = try await URLSession.shared.data(from: urlUnwrapped)
-            if let responseConverted = response as? HTTPURLResponse {
-                print ("status code :\(responseConverted.statusCode )")
-                let printableData: String = String(decoding: data, as: UTF8.self)
-                print(printableData)
-            }
-        } catch let error {
-            print(error)
-        }
-        
+func getStockDetail() async {
+    let urlStr = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=10&apikey=26YEL3PQC5ZHFB1P"
+    let url: URL? = URL(string: urlStr)
+    guard let urlUnwrapped = url else {
+        return
     }
-
-}
-.padding()
+    do {
+        let (data, response) = try await URLSession.shared.data(from: urlUnwrapped)
+        if let responseConverted = response as? HTTPURLResponse {
+            print ("status code :\(responseConverted.statusCode )")
+            let printableData: String = String(decoding: data, as: UTF8.self)
+            print(printableData)
+        }
+    } catch let error {
+        print(error)
+    }
 }
 
