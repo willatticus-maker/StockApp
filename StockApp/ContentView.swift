d2362ffc40ea2bd3d0dde8627ea75b7a7313aa6c
willatticus-maker <will.atticus@gmail.com>
2026-04-27 09:05:36 -0400

    Integreated API data.

--- StockApp/ContentView.swift
+++ StockApp/ContentView.swift
@@ -10,12 +10,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var apiData: String  = "Placeholder"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(apiData)
+            
+            
+                .task {
+                    let urlStr = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=10&apikey=26YEL3PQC5ZHFB1P"
+                    let url: URL? = URL(string: urlStr)
+                    guard let urlUnwrapped = url else {
+                        return
+                    }
+                    do {
+                        let (data, response) = try await URLSession.shared.data(from: urlUnwrapped)
+                        if let responseConverted = response as? HTTPURLResponse {
+                            print ("status code :\(responseConverted.statusCode )")
+                            let printableData: String = String(decoding: data, as: UTF8.self)
+                            print(printableData)
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                    
+                }
+        
         }
         .padding()
     }
